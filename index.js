0e51ae855c9501c56828e5768c7ceeaade64bf09
Shreya jain <103766584+Shreyasinghai@users.noreply.github.com>
2025-03-21 16:37:29 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -6,12 +6,28 @@ for (var i = 0; i<numberOfButtons; i++){
 
         var buttonInnerHTML = this.innerHTML;
 
-        switch (buttonInnerHTML) {
+        makeSound(buttonInnerHTML);
+
+        buttonAnimation(buttonInnerHTML);
+
+    });
+
+    document.addEventListener("keypress", function(event) {
+
+        makeSound(event.key);
+    
+        buttonAnimation(event.key);
+    
+      
+      });
+    
+    function makeSound(key) {
+        switch (key) {
             case "w":
                     var tom1 = new Audio('sounds/tom-1.mp3');
                     tom1.play();
                 break;
-
+    
             case "a":
                     var tom2 = new Audio('sounds/tom-2.mp3');
                     tom2.play();
@@ -38,16 +54,22 @@ for (var i = 0; i<numberOfButtons; i++){
                 break;
         
             case "l":
-                    var kick = new Audio('sounds/kick.mp3');
+                    var kick = new Audio('sounds/kick-bass.mp3');
                     kick.play();
                 break;
                
                 default:
                     console.log(buttonInnerHTML);
         }
+    
+    }
 
-    });
+    function buttonAnimation(currentKey){
+        var activeButton = document.querySelector("." + currentKey);
+        activeButton.classList.add("pressed");
+        setTimeout(function(){
+            activeButton.classList.remove("pressed");
+        }, 100)
+    }
 
 }
-
-
